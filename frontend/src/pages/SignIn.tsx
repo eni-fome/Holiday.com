8ca6aa4f0e519ad5eb30f15ce7334fcc04642942
eni-fome <fomealalor@gmail.com>
2024-03-11 05:18:28 +0100

    signout completed >fixed general ui for the login and register page

--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -1,8 +1,8 @@
 import { useForm } from 'react-hook-form';
-import { useMutation } from 'react-query';
+import { useMutation, useQueryClient } from 'react-query';
 import * as apiClient from '../api-client';
 import { useAppContext } from '../contexts/AppContext';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 export type SignInFormData = {
     email: string;
@@ -10,6 +10,7 @@ export type SignInFormData = {
 };
 
 const SignIn = () => {
+    const queryClient = useQueryClient();
     const { showToast } = useAppContext();
     const navigate = useNavigate();
     const {
@@ -21,6 +22,7 @@ const SignIn = () => {
     const mutation = useMutation(apiClient.signIn, {
         onSuccess: async () => {
             showToast({ message: 'Sign in Successful', type: 'SUCCESS' });
+            await queryClient.invalidateQueries('validateToken');
             navigate('/');
         },
         onError: (error: Error) => {
@@ -67,7 +69,13 @@ const SignIn = () => {
                     </span>
                 )}
             </label>
-            <span>
+            <span className="flex items-center justify-between">
+                <span className="text-sm">
+                    Not registered?{' '}
+                    <Link className="underline" to="/register">
+                        Create an account
+                    </Link>
+                </span>
                 <button
                     type="submit"
                     className="bg-blue-600 text-white p-2 font-bold hover:bg-blue-500 text-xl"
