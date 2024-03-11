63ed79f59694e11fafe7361c51284eccd95cc77d
eni-fome <fomealalor@gmail.com>
2024-03-11 05:18:28 +0100

    signout completed >fixed general ui for the login and register page

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,5 +1,5 @@
 import { useForm } from "react-hook-form";
-import { useMutation } from "react-query";
+import { useMutation, useQueryClient } from "react-query";
 import * as apiClient from "../api-client";
 import { useAppContext } from "../contexts/AppContext";
 import { useNavigate } from "react-router-dom";
@@ -14,6 +14,7 @@ export type RegisterFormData = {
 };
 
 const Register = () => {
+  const queryClient = useQueryClient()
   const navigate = useNavigate()
   const {showToast} = useAppContext();
   const {
@@ -26,6 +27,7 @@ const Register = () => {
   const mutation = useMutation(apiClient.register, {
     onSuccess: async () => {
       showToast({message: "Registration Successful", type: "SUCCESS"})
+      await queryClient.invalidateQueries("validateToken")
       navigate("/")
     },
     onError: (error: Error) => {
