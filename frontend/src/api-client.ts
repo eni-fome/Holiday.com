771c420ad50d842d8ea2df7618bed1b7e1a99cf5
eni-fome <fomealalor@gmail.com>
2024-03-11 04:46:51 +0100

    Sign-in done

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -1,31 +1,48 @@
-import { RegisterFormData } from "./pages/Register";
+import { RegisterFormData } from './pages/Register';
+import { SignInFormData } from './pages/SignIn';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || '';
 
 export const register = async (formData: RegisterFormData) => {
     const response = await fetch(`${API_BASE_URL}/api/users/register`, {
-      method: "POST",
-      credentials: "include",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(formData),
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
     });
-  
+
     const responseBody = await response.json();
-  
-    if (!response.ok) {
-      throw new Error(responseBody.message);
-    }
-  };
 
-  export const validateToken = async () => {
+    if (!response.ok) {
+        throw new Error(responseBody.message);
+    }
+};
+
+export const signIn = async (formData: SignInFormData) => {
+    const response = await fetch(`${API_BASE_URL}/api/auth/login`, {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+    });
+    const body = await response.json();
+    if(!response.ok) {
+      throw new Error(body.message);
+    }
+    return body
+};
+
+export const validateToken = async () => {
     const response = await fetch(`${API_BASE_URL}/api/auth/validate-token`, {
-      credentials: "include",
+        credentials: 'include',
     });
 
     if (!response.ok) {
-      throw new Error ("Token invalid")
+        throw new Error('Token invalid');
     }
-    return response.json()
-  }
+    return response.json();
+};
