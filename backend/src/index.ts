222bdb6ae9f04deb8be26066d29ed7822de68a11
eni-fome <fomealalor@gmail.com>
2024-03-13 14:44:49 +0100

    added render url in backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
     cors({
-        origin: process.env.FRONTEND_URL,
+        origin: process.env.FRONTEND_URL || "https://holiday-com.onrender.com",
         credentials: true,
     }),
 );
