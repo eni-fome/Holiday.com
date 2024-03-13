84750b645eb05876819a9cdd71d6fb134b82c57f
eni-fome <fomealalor@gmail.com>
2024-03-13 07:44:57 +0100

    removed rollup config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,13 +3,5 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    build: {
-        rollupOptions: {
-            input: 'dist/assets/index-DTCCDpOj.js',
-            output: {
-                entryFileNames: 'index.js',
-            },
-        },
-    },
     plugins: [react()],
 });
