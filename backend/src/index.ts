c3f3d9e892a3ae5acc884f85008f1b4b25d387e0
eni-fome <fomealalor@gmail.com>
2024-03-14 17:27:58 +0100

    USE CORS

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -22,7 +22,12 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors())
+app.use(
+    cors({
+        origin: process.env.FRONTEND_URL,
+        credentials: true,
+    }),
+);
 
 app.use(express.static(path.join(__dirname, '../../frontend/dist')));
 app.use('/api/auth', authRoutes);
