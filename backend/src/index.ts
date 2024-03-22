6e7e688f1e35da63eecaea24340590115c778f9e
eni-fome <fomealalor@gmail.com>
2024-03-22 09:23:31 +0100

    remove cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -23,7 +23,7 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors())
+// app.use(cors())
 //   cors({
 //     origin: process.env.FRONTEND_URL,
 //     credentials: true,
