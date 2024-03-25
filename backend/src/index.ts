718d098d96fdccd4d17ac1001087db0c4d16c833
eni-fome <fomealalor@gmail.com>
2024-03-25 13:14:57 +0100

    ...

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 cors({
-    origin: "*",
+    origin: "process.env.FRONTEND_URL",
     credentials: false,
 });
 
