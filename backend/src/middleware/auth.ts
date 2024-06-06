673c2515bfcfc029b14e737a9b4e26e4a36bd971
eni-fome <fomealalor@gmail.com>
2024-06-06 21:33:13 +0100

    moved jwt to localStorage

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import jwt, { JwtPayload } from "jsonwebtoken";
 
+
 declare global {
   namespace Express {
     interface Request {
@@ -10,7 +11,9 @@ declare global {
 }
 
 const verifyToken = (req: Request, res: Response, next: NextFunction) => {
-  const token = req.cookies["auth_token"];
+  const authHeader = req.header('Authorization');
+  const token = authHeader && authHeader.split(' ')[1]; 
+
   if (!token) {
     return res.status(401).json({ message: "Unauthorized: No token provided" });
   }
